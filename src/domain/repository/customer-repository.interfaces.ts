32da99f439b77aa2e962dab1c20e6663d0d4a8b7
Marco Silveira <marcosilveira39@gmail.com>
2022-11-10 20:18:00 -0300

    first commit

---
+++ src/domain/repository/customer-repository.interfaces.ts
@@ -0,0 +1,6 @@
+import Customer from "../entity/custumer";
+import RepositoryInterface from "./repository-interface";
+
+
+export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> {
+}
---
+++ src/domain/service/product.service.ts
@@ -0,0 +1,10 @@
+import Product from "../entity/product";
+
+export default class ProductService {
+
+    static increasePrice(product: Product[], percentage: number): void {
+        product.forEach(product => {
+            product.changePrice((product.price * percentage)/100 + product.price)
+        })
+    }
+}
